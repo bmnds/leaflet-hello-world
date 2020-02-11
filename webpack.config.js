7c81fde0afc15267a5542db0c0842ed47ea0ff31
Bruno Mendes <b.mendes00@gmail.com>
2020-02-10 21:09:17 -0400

    Generate index.html from template

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist"
   },
   module: {
     rules: [
@@ -30,8 +30,12 @@ module.exports = {
     ]
   },
   plugins: [
-    new CopyPlugin([
-      { from: 'leaflet-gpx/dist/*.png', to: 'dist/images' },
-    ]),
-  ],
+    new HtmlPlugin({
+      //hash: true,
+      template: 'src/index.html', //relative to root folder
+      filename: "index.html", //relative to dist folder
+      title: 'Leaflet.js',
+      myPageHeader: 'Map with Basemap, Table of Contents and GPX rendering',
+    })
+  ]
 };
